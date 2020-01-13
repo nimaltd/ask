12d5c3d48bba465f3b87108e66a512e375d02e31
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2020-01-13 09:47:20 +0330

    Delete RemoteDecoderUser.c

--- RemoteDecoderUser.c
+++
@@ -1,9 +0,0 @@
-
-#include "RemoteDecoder.h"
-
-
-void	RemoteDecoder_User_Detect(uint8_t *DecodeInByte,uint8_t DecodeLenInBit,uint8_t *RawBitTimeArray,uint8_t RawBitTimeArrayLen)
-{
-	HAL_GPIO_TogglePin(LED_GPIO_Port,LED_Pin);	
-}
-
